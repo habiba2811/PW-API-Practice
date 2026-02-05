1aa2fc02745984a5f8efbe8abaf0bcaf1c14d1b9
habiba2811 <habiba.hisham2811@gmail.com>
2026-02-05 16:08:07 +0200

    modify api request

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -7,11 +7,26 @@ test.beforeEach( async ({ page }) => {
       body: JSON.stringify(tags)
     })
   })
+
+  await page.route('*/**/api/articles*', async route => {
+    const response= await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = 'This is a test title'
+    responseBody.articles[0].description = 'this is a test description'
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+    
+
+  })
     
   await page.goto('https://conduit.bondaracademy.com/');
 })
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('this is a test description')
+
 });
 
